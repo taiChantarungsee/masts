9ff870e5f04bcaae6f115522b9ff695a558bed49
taiChantarungsee <tai.jantarungsee@gmail.com>
2017-09-10 21:46:13 +0100

    Frontend updates: Added value of total rend, added dictionary of tenants and number of masts displayed as list of values.Having trouble processing the date information because they can't be stored in the django model without altering the original data, since Django doesn't accept the format they are in.

--- mast_app/views.py
+++ mast_app/views.py
@@ -5,8 +5,8 @@ from datetime import datetime
 
 #Function to convert dates into the right format.
 #def monthToNum(tenant):
-#	tenant.lease_start_date = datetime.strptime(tenant.lease_start_date, '%d-%m-%y').strftime('%d-%m-%Y')
-#	tenant.lease_end_date =  datetime.strptime(tenant.lease_end_date, '%d-%m-%y').strftime('%d-%m-%Y')
+#	tenant.lease_start_date = datetime.strptime(tenant.lease_start_date, '%d-%b-%y').strftime('%d-%m-%Y')
+#	tenant.lease_end_date =  datetime.strptime(tenant.lease_end_date, '%d-%b-%y').strftime('%d-%m-%Y')
 #	tenant.save()
 
 def index(request):
@@ -15,7 +15,7 @@ def index(request):
 
 	#For displaying the total rent for all items in the list.
 	total_rent = CsvData.objects.aggregate(Sum('current_rent'))
-
+	total_rent_value = total_rent.get('current_rent__sum')
 	#Create a dictionary containing tenant names and a count of masts, to display as a list.
 	tenants = CsvData.objects.all()
 	dictionary = {}
@@ -26,20 +26,26 @@ def index(request):
 				number_of_masts += 1
 		dictionary[tenant.tenant_name] = [number_of_masts]
 
+	print(dictionary)
 	#List the data for rentals with lease dates from 1 june 1999 and 31 August 2007 with format of DD/MM/YYYY.
 	#Convert fields first.
-
+#	rental_dates = []
 #	for tenant in tenants:
-#		monthToNum(tenant)
+#		start_date = datetime.strptime(tenant.lease_start_date, '%d-%m-%y')
+#		start = datetime.strptime('01-06-1999', '%d-%m-%y')
+#		end = datetime.strptime('31-08-2007', '%d-%m-%y')
+#		if start_date > start and start_date < end:	
+#			rental_dates.append(tenant)
 
-	rental_dates = CsvData.objects.filter(lease_start_date__range=["01-06-1999", "31-08-2007"])
+	#For formatting dates in models correctly
+	#for tenant in tenants:
+	#	monthToNum(tenant)
 
 	#Send all the processed data to the context.
 	context = {
 		'rent_ascending':rent_ascending,
-		'total_rent':total_rent,
+		'total_rent_value':total_rent_value,
 		'dictionary':dictionary,
-		'rental_dates':rental_dates,
+		#'rental_dates':rental_dates,
 	}
-
 	return render(request, 'masts/index.html', context)
